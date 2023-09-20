84cf81a0dadca78180ab556f94aa13ee5d1bae44
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-20 22:20:15 +0100

    changed App layout and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import AboutMe from "./pages/Aboutme";
 import Contact from "./pages/Contact";
@@ -7,33 +7,35 @@ import Projects from "./pages/Projects";
 import Cv from "./pages/Cv";
 import Footer from "./pages/Footer";
 import Error from "./pages/Error";
+
+
 import "./App.css";
 
 export default function App() {
+  let routes = (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/aboutme" element={<AboutMe />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/cv" element={<Cv />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
   return (
     <div className="container">
-      <Router>
-        <nav>
-          <Link to="/">Home</Link>
-          {""}
-          <Link to="/aboutme" className="red">
-            AboutMe
-          </Link>
-          {""}
-          <Link to="/contact">Contact</Link>
-          {""}
-          <Link to="/projects">Projects</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/aboutme" element={<AboutMe />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/cv" element={<Cv />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <nav>
+        <a href="/">Home</a>
+        {""}
+        <a href="/aboutme" className="red">
+          AboutMe
+        </a>
+        {""}
+        <a href="/contact">Contact</a>
+        {""}
+        <a href="/projects">Projects</a>
+      </nav>
+      <Router>{routes}</Router> <Footer />
     </div>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,63 @@
 import React from "react";
+
 import { useNavigate } from "react-router-dom";
 
 export default function Homepage() {
   let navigate = useNavigate();
   return (
     <div>
-      homememememe
-      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
-      <br /> <br /> <br /> <br />
+      <h1> EVA NAGENGAST</h1>
+      <h2> Aspiring Front-End Developer</h2>
+      <br /> <br /> <br />{" "}
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <br /> <br /> <br /> <br /> <br />
+      <div className="row">
+        <div className="col">dad</div>
+        <div className="col">sadsa</div>
+      </div>{" "}
+      <br /> <br /> <br /> <br /> <br /> <br />
       <button
         onClick={() => {
-          navigate("../Cv");
+          navigate("./Cv");
         }}
       >
         Work experience
