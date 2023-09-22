8ea55e253963331c80cd26b6e2df45e7fa8192f7
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-22 23:26:04 +0100

    div animation with framer motion

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Footer from "./Footer";
 import TypeIt from "typeit-react";
-import { motion } from "framer-motion";
+import { motion, useAnimation, useInView } from "framer-motion";
 import "./Homepage.css";
 
-import { useNavigate } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
 
 export default function Homepage() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) { mainControls.start("visible")
+    }
+  }, [isInView]);
 
   let navigate = useNavigate();
   return (
@@ -14,9 +23,20 @@ export default function Homepage() {
       <div className="container">
         <div className="hero">
           <TypeIt className="heading">Eva Nagengast</TypeIt>
-          <h2> Aspiring Front-End Developer</h2>
-          <h3>based in London</h3>
-          <button>let's connect</button>
+          <motion.div
+            variants={{
+              noshow: { opacity: 0, y: 100 },
+              show: { opacity: 1, y: 0 },
+            }}
+            initial="noshow"
+            animate="show"
+            transition={{ duration: 2, delay: 1 }}
+            className="subheader"
+          >
+            <h2>Aspiring Front-End Developer</h2>
+            <h3>based in London</h3>
+            <button>let's connect</button>
+          </motion.div>
         </div>
         <main>
           <div className="child-one">
@@ -36,8 +56,17 @@ export default function Homepage() {
             <br /> txt txt
             <br /> txt
           </div>
-          <motion.div  initial={{ opacity: 0 }}
-  whileInView={{ opacity: 1 }}className="child-two">
+          <motion.div
+            ref={ref}
+            variants={{
+              hidden: { opacity: 0, y: 70 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={mainControls}
+            transition={{ duration: 1.5, delay: 0.5 }}
+            className="child-two"
+          >
             see about me txt
             <br /> txt txt
             <br /> txt txt
@@ -54,7 +83,7 @@ export default function Homepage() {
             <br /> txt txt
             <br /> txt
           </motion.div>
-          <div className="child-two">see about me</div>
+          <div className="child-three">see about me</div>
         </main>
         <button
           onClick={() => {
