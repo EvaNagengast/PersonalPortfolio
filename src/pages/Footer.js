c56916f3b963d7d447fb62ad18ef4c134b9cf326
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-22 12:55:36 +0100

    added typewriter library

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css"
 export default function Footer() {
   return (
     <footer className="Footer">
-      This page was crafted with care and built using JavaScript React, Bootstrap it is open-sourced on
+      This page was crafted with care and built using JavaScript React and is open-sourced on
       <a
         href="https://github.com/EvaNagengast/PersonalPortfolio"
         target="_blank"
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,29 @@
 import React from "react";
 import Footer from "./Footer";
+import TypeIt from "typeit-react";
 import "./Homepage.css";
 
 import { useNavigate } from "react-router-dom";
 
+
+
+
+
+
+
+
 export default function Homepage() {
+
+
   let navigate = useNavigate();
   return (
-    <div className="Homepage"><header>
-      <h1> Eva Nagengast</h1></header>
+    <div className="Homepage">
+      <div className="App">
+        <TypeIt className="heading">
+        
+         Eva Nagengast
+        </TypeIt>
+      </div>
       <h2> Aspiring Front-End Developer</h2>
       <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
       <div className="row">
