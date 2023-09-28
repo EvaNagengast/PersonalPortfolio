3d07d02b03964181ac9916486d0514bd214b3c7e
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-28 02:26:00 +0100

    fixed navbar and about me responsiveness

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -29,31 +29,30 @@ export default function Aboume() {
               My journey has been a winding road filled with diverse
               experiences, each contributing to my unique perspective and
               skillset. I've worn many hats and tackled a variety of roles,
-              excelling in each one. My colourful career began in the world of
-              mechatronics, where I delved into the intricacies of combining
-              mechanics and electronics. From there, I ventured into music
-              booking and hospitality, immersing myself in the vibrant world of
-              events and entertainment. <br />
+              excelling in each one. My colourful career began in the field of
+              mechatronics, where I explored the complexities of integrating
+              mechanics and electronics. From there, I ventured into my passion
+              music booking and hospitality. <br />
               <br />
               My pivotal moment came when I joined a company where I worked with
-              both artificial intelligence and audio technology. I had the
-              fascinating opportunity to transform AI audio into realistic,
-              human-like speech. This experience marked my entry into the world
-              of tech, a journey that continues to evolve. <br />
+              both artificial intelligence and audio technology. I had the great
+              opportunity to transform AI audio into realistic, human-like
+              speech. This experience marked my entry into the world of tech, a
+              journey that continues to evolve. <br />
               <br />
               Today, I'm a Junior Front-End Developer, and I've found my true
               passion in bringing digital experiences to life. Fueled by my
-              infectious enthusiasm, immense dedication, and unwavering passion
-              for frontend development, I constantly seek to create
-              user-friendly and visually stunning websites and apps. <br />
+              infectious enthusiasm, immense dedication, and passion for
+              frontend development, I seek to create user-friendly and visually
+              stunning websites and apps. <br />
               <br />
-              When I'm not deeply engrossed in a new project, you'll often find
-              me volunteering at local conservation sites, where I'm committed
-              to preserving our natural world. I'm also a regular presence at
-              music venues and gigs, attending concerts to savor the magic of
-              live music. Additionally, I take care of the local squirrel
-              population, a charming reminder of the beauty in our everyday
-              surroundings.
+              When I'm not deeply working on a new project, you'll often find me
+              volunteering at local conservation sites, where I'm committed to
+              preserving our local natural habitat. I frequently attend music
+              venues and gigs, making it a habit to go to concerts supoort the
+              local subculture . Additionally, I look after the local squirrel
+              and bird population, trying to take care of all members of our
+              community.
               <br />
               <br /> I'm excited for the future and can't wait to have a chat
               about your ideas and projects
@@ -62,7 +61,7 @@ export default function Aboume() {
               onClick={() => {
                 navigate("../contact");
               }}
-              className=" btn-base-dark px-3 py-1 mx-3 mb-4 mt-2"
+              className=" btn-base-dark px-3 py-1 ms-1 me-3 mx-sm-3 mb-4 mt-2"
             >
               CONTACT ME
             </button>
