c240596d0b83853f3466698236a55f850503592c
SpiceWeasel66 <nagengasteva@gmail.com>
2024-01-30 20:25:39 +0000

    updated cv content

--- src/pages/Cv.js
+++ src/pages/Cv.js
@@ -1,34 +1,36 @@
-import React from "react";
-import TypeIt from "typeit-react";
-import monoimage from "../media/monoimg.png";
-import pdf from "../media/EvaNagengastResume.pdf";
-import "./Cv.css";
+import React from 'react';
+import TypeIt from 'typeit-react';
+import monoimage from '../media/monoimg.png';
+import pdf from '../media/EvaNagengastResume.pdf';
+import './Cv.css';
 
 export default function Cv() {
   return (
-    <div className="Cv">
-      <div className="cvpage">
+    <div className='Cv'>
+      <div className='cvpage'>
         <a
           href={pdf}
-          alt="PDF of Evas Resume"
-          className="mx-auto mt-4 mb-2 px-3 py-2 btn-dark-base downloadbutton"
-        >
+          alt='PDF of Evas Resume'
+          className='mx-auto mt-4 mb-2 px-3 py-2 btn-dark-base downloadbutton'>
           CLICK HERE FOR PDF-View
         </a>
         <main>
-          <div className="row justify-content-between  ">
-            <div className="col-12 col-md order-2 order-md-1">
+          <div className='row justify-content-between  '>
+            <div className='col-12 col-md order-2 order-md-1'>
               <h1>
                 Eva <br />
                 Nagengast
               </h1>
             </div>
-            <div className="col-12 col-md  order-1 order-md-2">
-              <div className="mt-2 mt-lg-4 me-0 me-lg-4 float-md-end cvimage">
-                <img src={monoimage} alt="Eva Nagengast" />
-                <div className="imgcaption">
-                  <span className="subheader">I'm </span>
-                  <span className="highlightred">
+            <div className='col-12 col-md  order-1 order-md-2'>
+              <div className='mt-2 mt-lg-4 me-0 me-lg-4 float-md-end cvimage'>
+                <img
+                  src={monoimage}
+                  alt='Eva Nagengast'
+                />
+                <div className='imgcaption'>
+                  <span className='subheader'>I'm </span>
+                  <span className='highlightred'>
                     <TypeIt
                       options={{
                         speed: 150,
@@ -39,25 +41,25 @@ export default function Cv() {
                       getBeforeInit={(instance) => {
                         instance
 
-                          .type("a coder.")
+                          .type('a coder.')
                           .pause(1000)
                           .delete(8)
                           .pause(700)
-                          .type("living in London.")
+                          .type('living in London.')
                           .pause(1000)
                           .delete(17)
                           .pause(700)
-                          .type("from Germany.")
+                          .type('from Germany.')
 
                           .pause(1000)
                           .delete(13)
                           .pause(700)
-                          .type("loving music.")
+                          .type('loving music.')
                           .pause(1000)
                           .delete(13)
                           .pause(700)
 
-                          .type("Eva.");
+                          .type('Eva.');
                         return instance;
                       }}
                     />
@@ -67,41 +69,90 @@ export default function Cv() {
             </div>
           </div>
           <h2> Career Summary</h2>
-          <div className="row mb-2 mb-md-0 pt-1 ">
-            <div className="col-12 col-md-9 ">
-              <h3 className="mb-0">AI Audio Content Evaluator - Linguist</h3>
+          <div className='row mb-2 mb-md-0 pt-1 '>
+            <div className='col-12 col-md-9 '>
+              <h3 className='mb-0'>Freelance</h3>
             </div>
-            <div className="col-12 col-md-3 text-start text-md-end">
-              <h3 className="mb-0">From Apr 23</h3>
+            <div className='col-12 col-md-3'>
+            
             </div>
           </div>
-          <h4 className="mb-2">DeepZen lt. - Freelance - Remote</h4>
-
+          <div className='row mb-2 mb-md-0 pt-1 '>
+            <div className='col-12 col-md-9 '>
+              <h4 className='mb-0'>
+                Front End Development / Website Maintenance
+              </h4>
+            </div>
+            <div className='col-12 col-md-3 text-start text-md-end'>
+              <h4 className='mb-0'>since Nov 23</h4>
+            </div>
+          </div>
           <ul>
             <li>
-              Use of a specialised program to transform AI-generated audio into
-              realistic human-like speech
+              Developing and designing customised, responsive, user-friendly and
+              beautiful websites using HTML, CSS, JavaScript and React.js
             </li>
             <li>
-              Analysing, improving and fine-tuning the audio files with the
-              DeepZen Studio
+              Optimising websites with focus on clients visions and missions,
+              prioritising high-quality standards and deadlines
             </li>
             <li>
-              Making nuanced adjustments based on understanding of tone, pacing
-              and inflection
+              Collaborating on commercial websites with frameworks like Next.js,
+              or responsive frameworks like Bootstrap
+            </li>
+            <li>
+              Writing clean and maintainable code, incorporating industry
+              standards, continuously improving projects
             </li>
           </ul>
-
-          <div className="row mb-2 mb-md-0 pt-3">
-            <div className="col-12 col-md-8">
-              <h3 className="mb-0">Plant Technician</h3>
+          <div className='row mb-2 mb-md-0 pt-1 '>
+            <div className='col-12 col-md-9 '>
+              <h4 className='mb-0'>Audio Content Evaluator, Deep Zen</h4>
             </div>
-            <div className="col-12 col-md-4 text-start text-md-end">
-              <h3 className="mb-0">May 22 - Mar 23</h3>
+            <div className='col-12 col-md-3 text-start text-md-end'>
+              <h4 className='mb-0'>Apr 23 - Dec 23</h4>
             </div>
           </div>
-          <h4 className="mb-2">Plant Plan - Leicester/London</h4>
-
+          <ul>
+            <li>
+              Use of specialised programs to transform AI-generated audio into
+              human-like speech patterns
+            </li>
+            <li>
+              Analysing, improving and fine-tuning audio files, audio libraries
+              and collections
+            </li>
+          </ul>{' '}
+          <div className='row mb-2 mb-md-0 pt-1 '>
+            <div className='col-12 col-md-9 '>
+              <h4 className='mb-0'>
+                {' '}
+                Translator German/English, Box To Box Films
+              </h4>
+            </div>
+            <div className='col-12 col-md-3 text-start text-md-end'>
+              <h4 className='mb-0'> Mar 20 – Jul 21</h4>
+            </div>
+          </div>
+          <ul>
+            <li>
+              Adaptation and translation of the subtitles of “Formula 1: Drive
+              to Survive” from German to English
+            </li>
+            <li>
+              Collaborating with project managers to meet strict deadlines and
+              ensure high quality translations
+            </li>
+          </ul>
+          <div className='row mb-2 mb-md-0 pt-3'>
+            <div className='col-12 col-md-8'>
+              <h3 className='mb-0'>Plant Technician</h3>
+            </div>
+            <div className='col-12 col-md-4 text-start text-md-end'>
+              <h3 className='mb-0'>May 22 - Mar 23</h3>
+            </div>
+          </div>
+          <h4 className='mb-2'>Plant Plan - Leicester/London</h4>
           <ul>
             <li>
               Visited clients to maintain live plant displays, to give advise
@@ -109,27 +160,25 @@ export default function Cv() {
             </li>
             <li>
               Managed schedules, ordered plants and materials, and solved
-              practical challenges on-site{" "}
+              practical challenges on-site{' '}
             </li>
             <li>
               Liaised professionally with customers and communicated with
               managers, and stakeholders
             </li>
           </ul>
-
-          <div className="row mb-2 mb-md-0 pt-3">
-            <div className="col-12 col-md-8">
-              <h3 className="mb-0">Assistant General Manager</h3>
+          <div className='row mb-2 mb-md-0 pt-3'>
+            <div className='col-12 col-md-8'>
+              <h3 className='mb-0'>Assistant General Manager</h3>
             </div>
-            <div className="col-12 col-md-4 text-start text-md-end">
-              <h3 className="mb-0">Aug 18 - Apr 22</h3>
+            <div className='col-12 col-md-4 text-start text-md-end'>
+              <h3 className='mb-0'>Aug 18 - Apr 22</h3>
             </div>
           </div>
-          <h4 className="mb-2">Honest Burgers - London</h4>
-
+          <h4 className='mb-2'>Honest Burgers - London</h4>
           <ul>
             <li>
-              {" "}
+              {' '}
               Offered a homely experience for customers, with an
               old-school-hospitality mindset
             </li>
@@ -146,19 +195,17 @@ export default function Cv() {
               external contractors
             </li>
           </ul>
-
-          <div className="row mb-2 mb-md-0 pt-3">
-            <div className="col-12 col-md-8">
-              <h3 className="mb-0">Tour-Booking Assistant</h3>
+          <div className='row mb-2 mb-md-0 pt-3'>
+            <div className='col-12 col-md-8'>
+              <h3 className='mb-0'>Tour-Booking Assistant</h3>
             </div>
-            <div className="col-12 col-md-4 text-start text-md-end">
-              <h3 className="mb-0">Jan 18 - Jul 18</h3>
+            <div className='col-12 col-md-4 text-start text-md-end'>
+              <h3 className='mb-0'>Jan 18 - Jul 18</h3>
             </div>
           </div>
-          <h4 className="mb-2">
+          <h4 className='mb-2'>
             Destiny Tourbooking - Berlin (6 Month contract)
           </h4>
-
           <ul>
             <li>
               Collaborated with bands and promoters, efficiently exchanging tour
@@ -166,24 +213,22 @@ export default function Cv() {
             </li>
             <li>
               Drafted contracts and invoices, frequently releasing pre-sale
-              numbers and show announcements{" "}
+              numbers and show announcements{' '}
             </li>
             <li>
               Developed an internal data exchange system to save time and
               improve workflow
             </li>
           </ul>
-
-          <div className="row mb-2 mb-md-0 pt-3">
-            <div className="col-12 col-md-8">
-              <h3 className="mb-0">Production Shift Manager, Team Leader</h3>
+          <div className='row mb-2 mb-md-0 pt-3'>
+            <div className='col-12 col-md-8'>
+              <h3 className='mb-0'>Production Shift Manager, Team Leader</h3>
             </div>
-            <div className="col-12 col-md-4 text-start text-md-end">
-              <h3 className="mb-0"> Sep 08 - Dec 17</h3>
+            <div className='col-12 col-md-4 text-start text-md-end'>
+              <h3 className='mb-0'> Sep 08 - Dec 17</h3>
             </div>
           </div>
-          <h4 className="mb-2">Robert Bosch GmbH - Bamberg</h4>
-
+          <h4 className='mb-2'>Robert Bosch GmbH - Bamberg</h4>
           <ul>
             <li>
               Led large teams of up to 40 people, providing support, direction
@@ -202,110 +247,116 @@ export default function Cv() {
               through active research and SAP.
             </li>
           </ul>
-
-          <h2 className="pt-3">Projects</h2>
-          <div className="row">
-            <div className="col-12 col-sm-8">
-              <h4 className="pt-2 mb-0">
-                {" "}
-                Volunteering conservation work, nature education{" "}
+          <h2 className='pt-3'>Projects</h2>
+          <div className='row'>
+            <div className='col-12 col-sm-8'>
+              <h4 className='pt-2 mb-0'>
+                {' '}
+                Volunteering conservation work, nature education{' '}
               </h4>
             </div>
-            <div className="col-12 col-sm-4 text-end">
-              <h4 className="pt-0 text-start text-sm-end"> From 2023 </h4>
+            <div className='col-12 col-sm-4 text-end'>
+              <h4 className='pt-0 text-start text-sm-end'> From 2023 </h4>
             </div>
           </div>
-          <p className="mt-0 pt-md-1">
+          <p className='mt-0 pt-md-1'>
             Creekside Discovery Center, Nature Conservation- London
           </p>
-          <div className="row pt-2">
-            <div className="col-12 col-sm-8">
-              <h4 className="pt-2 mb-0">
-                {" "}
+          <div className='row pt-2'>
+            <div className='col-12 col-sm-8'>
+              <h4 className='pt-2 mb-0'>
+                {' '}
                 Founder, Distribution, Artist support, Accounting
               </h4>
             </div>
-            <div className="col-12 col-sm-4 text-end">
-              <h4 className="pt-0 text-start text-sm-end"> 2014 - 2017 </h4>
+            <div className='col-12 col-sm-4 text-end'>
+              <h4 className='pt-0 text-start text-sm-end'> 2014 - 2017 </h4>
             </div>
           </div>
-          <p className="mt-0 pt-md-1">Raptor Records, Label - Nuremberg</p>
-          <div className="row pt-2">
-            <div className="col-12 col-sm-8">
-              <h4 className="pt-2 mb-0">
+          <p className='mt-0 pt-md-1'>Raptor Records, Label - Nuremberg</p>
+          <div className='row pt-2'>
+            <div className='col-12 col-sm-8'>
+              <h4 className='pt-2 mb-0'>
                 Event Management, local culture and music support
               </h4>
             </div>
-            <div className="col-12 col-sm-4 text-end">
-              <h4 className="pt-0 text-start text-sm-end"> 2013 - 2016 </h4>
+            <div className='col-12 col-sm-4 text-end'>
+              <h4 className='pt-0 text-start text-sm-end'> 2013 - 2016 </h4>
             </div>
           </div>
-          <p className="mt-0 pt-md-1">H&S Subculture events - Nuremberg </p>
-
-          <div className="sideSection">
-            <h2 className="pt-3"> Education</h2>
-            <div className="row pt-2">
-              <div className="col-12 col-sm-8 ">
-                <h4 className="mb-0"> SheCodes</h4>
+          <p className='mt-0 pt-md-1'>H&S Subculture events - Nuremberg </p>
+          <div className='sideSection'>
+            <h2 className='pt-3'> Education</h2>
+            <div className='row pt-2'>
+              <div className='col-12 col-sm-8 '>
+                <h4 className='mb-0'> SheCodes</h4>
               </div>
-              <div className="col-12 col-sm-4 text-end">
-                <h4 className="mb-1 mb-sm-0 text-start text-sm-end">
-                  Jul - Oct 23
+              <div className='col-12 col-sm-4 text-end'>
+                <h4 className='mb-1 mb-sm-0 text-start text-sm-end'>2023</h4>
+              </div>
+            </div>
+            <p>
+              Certificate in Front-End Development - HTML, CSS, JavaScript,
+              React.js
+            </p>
+            <div className='row pt-2'>
+              <div className='col-12 col-sm-8 pt-3 pt-sm-2'>
+                <h4 className='mb-0'> Bosch GmbH </h4>
+              </div>
+              <div className='col-12 col-sm-4 text-end'>
+                <h4 className='mb-1 mb-sm-0 text-start text-sm-end'>
+                  2005 - 2008
                 </h4>
               </div>
             </div>
-            <p>Certificate in Front-End Development</p>
+            <p>BTEC + Apprenticeship</p>
 
-            <div className="row pt-2">
-              <div className="col-12 col-sm-8 pt-3 pt-sm-2">
-                <h4 className="mb-0"> Bosch GmbH </h4>
+            <div className='row pt-2'>
+              <div className='col-12 col-sm-8 pt-3 pt-sm-2'>
+                <h4 className='mb-0'>Adalbert Raps Schule </h4>
               </div>
-              <div className="col-12 col-sm-4 text-end">
-                <h4 className="mb-1 mb-sm-0 text-start text-sm-end">
-                  2008-2012
+              <div className='col-12 col-sm-4 text-end'>
+                <h4 className='mb-1 mb-sm-0 text-start text-sm-end'>2008</h4>
+              </div>
+              <p>A-levels </p>
+            </div>
+            <div className='row pt-2'>
+              <div className='col-12 col-sm-8 pt-3 pt-sm-2'>
+                <h4 className='mb-0'>Realschule Hirschaid </h4>
+              </div>
+              <div className='col-12 col-sm-4 text-end'>
+                <h4 className='mb-1 mb-sm-0 text-start text-sm-end'>
+                  until 2005
                 </h4>
               </div>
             </div>
-            <p>BTEC</p>
-            <p> Apprenticeship + A-levels</p>
-            <div className="row pt-2">
-              <div className="col-12 col-sm-8 pt-3 pt-sm-2">
-                <h4 className="mb-0">Realschule Hirschaid </h4>
-              </div>
-              <div className="col-12 col-sm-4 text-end">
-                <h4 className="mb-1 mb-sm-0 text-start text-sm-end">
-             until     2008 
-                </h4>
-              </div>
-            </div>
-      
-            <p>GCSEs (Language, Math, German, English)</p>
-            <h2 className="pt-3">SKILLS/TOOLS</h2>
-            <ul className="ps-1">
-              <li>HTML + CSS</li>
-              <li>JavaScript</li>
+            <p>GCSEs </p>
+            <h2 className='pt-3'>SKILLS</h2>
+            <ul className='ps-1'>
+              <li>Front-End Development - HTML + CSS, JavaScript, React.js</li>
+              <li>Version control - Git, GitHub</li>
 
-              <li>React</li>
-              <li>VS Code </li>
-              <li>Bootstrap and Responsive Development </li>
-              <li>Figma</li>
-              <li>Libraries and APIs </li>
-              <li>Adobe Illustrator, Fresco, Photoshop, Lightroom</li>
+              <li>Responsive and Agile Web Development</li>
+              <li>Frameworks - Next.js, Bootstrap</li>
+              <li>CMS, Social Media, SEO</li>
+              <li>Google Workspace, MS Office</li>
+              <li> Adobe - PhotoShop, Illustrator, Fresco, Lightroom</li>
             </ul>
-            <h2 className="pt-3">Languages</h2>
-            <ul className="ps-1">
+            <h2 className='pt-3'>Languages</h2>
+            <ul className='ps-1'>
               <li>German - Native </li>
               <li>English - Bilingual Proficiency</li>
               <li>Spanish - Beginner</li>
             </ul>
-            <h2 className="pt-3">Strengths</h2>
-            <ul className="ps-1">
-              <li>Front-End Development</li>
-              <li>Project Management </li>
-              <li>Creativity </li>
+            <h2 className='pt-3'>Strengths</h2>
+            <ul className='ps-1'>
               <li>Excellent Communication</li>
+              <li>Quick Perception</li>
               <li>Adaptability</li>
-              <li>Organisational skills</li>
+              <li>Ingenuity and Creativity</li>
+              <li>Community- and Team-spirit</li>
+              <li>Organisational Skills </li>
+              <li>Workflow Efficiency</li>
             </ul>
           </div>
         </main>
